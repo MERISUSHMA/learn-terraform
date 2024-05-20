25ce06fc6ae291eba2eeb9876ec6832ba6849138
HP <ammuraj4466@gmail.com>
2024-05-20 12:06:32 +0530

    terraform variables

---
+++ 03-loops/main.tf
@@ -0,0 +1,32 @@
+# loops in terraform can be done in 2 ways
+# 1. count
+# 2. for_each
+
+# count is used if your input is list
+# for_each is used if your input is map
+
+variable "instances" {
+  default = ["frontend", "backend", "mysql"]
+}
+
+resource "null_resource" "sample" {
+  count = length(var.instances)
+}
+
+variable "instances1" {
+  default = {
+
+    frontend = {
+      name = "frontend"
+      instance_type = "t3.micro"
+    }
+    backend = {
+      name = "backend"
+      instance_type = "t3.micro"
+    }
+    mysql = {
+      name = "mysql"
+      instance_type = "t3.micro"
+    }
+  }
+}
