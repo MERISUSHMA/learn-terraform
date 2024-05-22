e83d08b4d27a3a3bf10a5f4f374933f74f0d323b
HP <ammuraj4466@gmail.com>
2024-05-22 19:50:40 +0530

    terraform variables

--- expense-v4/dev.tfvars
+++ expense-v4/dev.tfvars
@@ -4,15 +4,15 @@ zone_id = "Z06703532AGGGEDYFVXSW"
 sg_id = ["sg-080e84444fa0ee87c"]
 components = {
   frontend = {
-    name          = "frontend"
+    name = "frontend"
     instance_type = "t3.micro"
   }
   backend = {
-    name          = "backend"
+    name ="backend"
     instance_type = "t3.micro"
   }
   mysql = {
-    name          = "mysql"
+    name = "mysql"
     instance_type = "t3.micro"
   }
 }
--- expense-v4/module/main.tf
+++ expense-v4/module/main.tf
@@ -1,20 +1,18 @@
 resource "aws_instance" "instance" {
-  ami = var.ami
-  instance_type = var.instance_type
-  vpc_security_group_ids = var.sg_id
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  vpc_security_group_ids = vars.sg_id
   tags = {
-    Name = "${var.name}-${var.env}"
+    name = "${var.name}-${var.env}"
   }
-
 }
 
-resource "aws_route53_record" "record" {
-  ami = var.ami
-  zone_id =var.zone_id
-  name = "${var.name}-${var.env}"
-  type = "A"
-  ttl = 30
-  records = [aws_instance.instances.private_ip]
+resource "aws_route53_record" "frontend" {
+  zone_id  =var.zone_id
+  name     = "${var."name"}-${var.env}"
+  type     = "A"
+  ttl      = 30
+  records = [aws_instance.instance.private_ip]
 }
 
 
