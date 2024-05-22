ab287b4a51771ca614df511a6146d651b144cdf8
HP <ammuraj4466@gmail.com>
2024-05-22 19:58:52 +0530

    terraform variables

--- expense-v4/module/main.tf
+++ expense-v4/module/main.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "instance" {
 
 resource "aws_route53_record" "record" {
   zone_id  =var.zone_id
-  name     = "${var."name"}-${var.env}"
+  name     = "${var.name}-${var.name}"
   type     = "A"
   ttl      = 30
   records = [aws_instance.instance.private_ip]
